c044cc6f740219050c2200cc48a733e5c18ec00d
Harsh <Harshdevsingh1992@gmail.com>
2023-02-19 23:18:06 +0000

    sqllite pipeline added

--- quotesHtml/pipelines.py
+++ quotesHtml/pipelines.py
@@ -5,9 +5,21 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sqlite3
 
 
 class QuoteshtmlPipeline:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('quotes.db')
+        self.conn.execute('''CREATE TABLE quotes
+                     (author TEXT, quote TEXT)''')
+        self.conn.commit()
+
     def process_item(self, item, spider):
+        self.conn.execute("""
+                    INSERT INTO quotes (author, quote) VALUES (?, ?)
+                """,
+                          (item['author'], item['quote']))
+        self.conn.commit()
         return item
